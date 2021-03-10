3cb0a2ccdb2c0c2ac81667a4d7acaa93b3692372
CynthiaTu-SY <57238251+CynthiaTu-SY@users.noreply.github.com>
2021-03-10 13:19:42 -0800

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -2,6 +2,8 @@ from functions import *
 import sys, argparse
 
 def encoder(msg, key):
+    #input validation has to be check here, otherwise the exit commond cannot pass such far away
+    DEPTH, N = validate(msg, key)
     repeat = key[1]
     for i in range(0, repeat):
         msg = encrypt(msg, key)
